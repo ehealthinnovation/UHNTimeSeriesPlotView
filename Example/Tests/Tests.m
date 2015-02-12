af0bd3d3eb00e19613a0670d19aa82b53a81e52c
Nathaniel Hamming <nhamming@ehealthinnovation.org>
2015-02-12 10:17:24 -0400

    Added basic unit tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,37 +6,74 @@
 //  Copyright (c) 2014 Nathaniel Hamming. All rights reserved.
 //
 
-SpecBegin(InitialSpecs)
+#import <UHNTimeSeriesPlotView/UHNTimeSeriesPlotView.h>
 
-describe(@"these will fail", ^{
+SpecBegin(PlotSpecs)
 
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
+describe(@"Testing plotting points and updating style", ^{
 
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
+    __block UHNGraphGridLines *grid = nil;
+    __block UHNScrollingTimeSeriesPlotView *plot = nil;
+    __block UIView *container = nil;
     
-    it(@"will wait and fail", ^AsyncBlock {
+    beforeEach(^{
+        container = [[UIView alloc] initWithFrame: CGRectMake(0, 0, 302, 202)];
+        container.backgroundColor = [UIColor blackColor];
         
+        plot = [[UHNScrollingTimeSeriesPlotView alloc] initWithFrame: CGRectMake(0, 0, 300, 200)];
+        grid = [[UHNGraphGridLines alloc] initWithFrame: CGRectMake(0, 0, 300, 200)];
+        grid.backgroundColor = [UIColor clearColor];
+        grid.graph = plot;
+        
+        plot.grid = grid;
+        [plot setupPlotWithXAxisMin: 0.
+                           xAxisMax: 30.
+                         xMinorStep: 2
+                         xMajorStep: 10.
+                         xAxisLabel: nil
+                  xAxisFormatString: nil
+                           yAxisMin: 0.
+                           yAxisMax: 20.
+                         yMinorStep: 5.
+                         yMajorStep: 10.
+                         yAxisLabel: nil
+                  yAxisFormatString: nil
+                          gridColor: [UIColor grayColor]
+                     gridFrameWidth: 1.
+                      drawGridFrame: NO
+                  fadeGridLineEdges: YES
+                          lineColor: [UIColor whiteColor]
+                      lineHeadColor: [UIColor blueColor]
+                       andLineWidth: 1.];
+        plot.plotRefreshRateInHz = 1;
+        plot.samplingRateInHz = 1;
+        plot.windowMaxSize = 60;
+        plot.backgroundColor = [UIColor clearColor];
+        
+        plot.center = container.center;
+        grid.center = container.center;
+        [container addSubview: plot];
+        [container addSubview: grid];
     });
-});
-
-describe(@"these will pass", ^{
     
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
+    it(@"plot can present data", ^{
+        NSArray *points = @[@2., @4., @6., @8., @10., @12., @14., @16., @18., @20., @18., @16., @14., @12., @10., @8., @6., @4., @2.];
+        [plot plotData: points];
+        expect(container).to.haveValidSnapshot();
     });
     
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^AsyncBlock {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            done();
-        });
+    it(@"plot and grid style can change", ^{
+        NSArray *points = @[@2., @4., @6., @8., @10., @12., @14., @16., @18., @20., @18., @16., @14., @12., @10., @8., @6., @4., @2.];
+        [plot plotData: points];
+        plot.lineColor = [UIColor blueColor];
+        plot.lineHeadColor = [UIColor yellowColor];
+        plot.lineWidth = 5.;
+        grid.frameColor = [UIColor orangeColor];
+        grid.gridLineColor = [UIColor redColor];
+        grid.frameLineWidth = 3.;
+        grid.fadeGridLineEdges = NO;
+        grid.drawsFrame = YES;
+        expect(container).to.haveValidSnapshot();
     });
 });
 
